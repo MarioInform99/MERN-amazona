c2683fd612d9c7f1b74d340817c8180de521fa33
PAPI131099 <58227775+PAPI131099@users.noreply.github.com>
2022-05-22 21:05:10 +0200

    Inicio de proyecto MERN

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        <a href="/">amazona</a>
+      </header>
+      <main>List products</main>
+    </div>
+  );
+};
+
+export default App;
